5cebf4e771f134fdb39dbf47d255c4302cd3cff7
Megan <megan.stodel@bbc.co.uk>
2020-05-14 09:20:53 +0100

    Add hello world

---
+++ hello_world.R
@@ -0,0 +1,24 @@
+## Print a greeting
+
+print("Hello World")
+sdada
+## To find out more about a function:
+
+?print
+help(print)
+
+## You can create and reference objects
+
+greeting <- "Hello World"
+
+print(greeting)
+
+# R is case sensitive
+
+print(Greeting)
+
+# Make sure you close brackets
+
+print(greeting
+      
+      
